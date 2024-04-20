a04f2a6ec637c3321a8a4480d3f8574e51263827
Vitto Payver <166740894+vittopayver@users.noreply.github.com>
2024-04-20 13:00:43 -0300

    cards.js

--- cards.js
+++ cards.js
@@ -9,7 +9,7 @@
 
         function zap2() {
             
-            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app/1713627644213.png"%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 310%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2014%2C99%0D%0A%0D%0A";
+            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app/1713627644213.png%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 310%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2014%2C99%0D%0A%0D%0A";
             window.open(url, "_blank");
         }
         
