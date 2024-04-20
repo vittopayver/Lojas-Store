be5f0821d460674b64a4d26269a293af76a2ccb1
Vitto Payver <166740894+vittopayver@users.noreply.github.com>
2024-04-20 13:58:09 -0300

    cards.js

--- cards.js
+++ cards.js
@@ -20,3 +20,30 @@
             var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 310%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2014%2C99%0D%0A%0D%0A";
             window.open(url, "_blank");
         }
+
+
+
+
+        function zap4() {
+            
+            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 310%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2014%2C99%0D%0A%0D%0A";
+            window.open(url, "_blank");
+         }
+
+
+
+
+         function zap5() {
+            
+            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 310%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2014%2C99%0D%0A%0D%0A";
+            window.open(url, "_blank");
+         }
+
+
+
+
+          function zap6() {
+            
+            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 310%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2014%2C99%0D%0A%0D%0A";
+            window.open(url, "_blank");
+          }
