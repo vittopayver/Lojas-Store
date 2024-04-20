b04e963a822b5060b114f31b4503254987cdfa68
Vitto Payver <166740894+vittopayver@users.noreply.github.com>
2024-04-20 17:19:07 -0300

    cards.js

--- cards.js
+++ cards.js
@@ -17,7 +17,7 @@
 
         function zap3() {
             
-            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 310%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2014%2C99%0D%0A%0D%0A";
+            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 520%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2024%2C99%0D%0A%0D%0A";
             window.open(url, "_blank");
         }
 
@@ -26,7 +26,7 @@
 
         function zap4() {
             
-            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 310%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2014%2C99%0D%0A%0D%0A";
+            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 1060%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2059%2C99%0D%0A%0D%0A";
             window.open(url, "_blank");
          }
 
@@ -35,7 +35,7 @@
 
          function zap5() {
             
-            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 310%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2014%2C99%0D%0A%0D%0A";
+            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 2180%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2094%2C99%0D%0A%0D%0A";
             window.open(url, "_blank");
          }
 
@@ -44,6 +44,6 @@
 
           function zap6() {
             
-            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 310%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2014%2C99%0D%0A%0D%0A";
+            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 5600%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%20114%2C99%0D%0A%0D%0A";
             window.open(url, "_blank");
           }
