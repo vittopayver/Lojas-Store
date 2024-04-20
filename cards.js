28d7f9f5536537c06590c1bdc283786e2f6ec389
Vitto Payver <166740894+vittopayver@users.noreply.github.com>
2024-04-20 11:52:57 -0300

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,22 @@
+
+        function zap1() {
+            
+            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 100%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%209%2C99%0D%0A%0D%0A";
+            window.open(url, "_blank");
+        }
+
+
+
+        function zap2() {
+            
+            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 310%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2014%2C99%0D%0A%0D%0A";
+            window.open(url, "_blank");
+        }
+        
+        
+
+        function zap3() {
+            
+            var url = "https://wa.me/5599984016496?text=https://lojasstore.netlify.app%20%20%20%20%20%20%20%20%20%0D%0A%0D%0AFree Fire%3A%0D%0A- 310%3A%20Diamantes%20%20%20%0A- 10%%3A%20de%20Bônus%0D%0A%0D%0APreço%3A%20R%24%2014%2C99%0D%0A%0D%0A";
+            window.open(url, "_blank");
+        }
---
+++ popup.js
@@ -0,0 +1,26 @@
+   // Lista de nomes de pessoas que compraram algo
+  var nomes = 
+  ["Alguém Comprou 100 +10", 
+  "Alguém Comprou 310 +31", 
+  "Alguém Comprou 520 +52", 
+  "Alguém Comprou 1060 +106", 
+  "Alguém Comprou 2180 +218", 
+  "Alguém Comprou 5600 +560"];
+
+  // Função para exibir uma notificação com nome aleatório a cada 10 segundos
+  function mostrarNotificacao() {
+    var nomeAleatorio = nomes[Math.floor(Math.random() * nomes.length)];
+    var mensagem = nomeAleatorio + " Diamantes e Ganhou 10% de Bônus";
+    var notificationElement = document.getElementById("notification");
+    notificationElement.textContent = mensagem;
+    notificationElement.style.display = "block";
+    setTimeout(function() {
+      notificationElement.style.display = "none";
+    }, 8000); // Ajuste o tempo de exibição da notificação aqui (em milissegundos)
+  }
+
+  // Chamar a função inicialmente
+  mostrarNotificacao();
+
+  // Chamar a função a cada 10 segundos
+  setInterval(mostrarNotificacao, 15000); // 10 segundos em milissegundos
